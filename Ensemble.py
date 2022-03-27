44cbb1e045ef5fbf797e9c0340329f75e73e7d22
bhanuteja2001 <k.bhanuteja2001@gmail.com>
2022-03-27 11:18:12 +0530

    first commit

---
+++ Ensemble.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from keras.models import load_model
+
+model = load_model("./weights/dense_NET.h5")
+model_2 = load_model("./weights/inception_v3.h5")
+
+class Ensemble:
+
+    #Image Path - absolute path of the Input Image
+    
+    def Result(self, img_array):
+        #plt.imshow(img_array)
+        #plt.show()
+        
+        #128 DenseNet
+        new_img_array = cv2.resize(img_array, (128, 128))
+        #new_img_array = img_array.resize((128,128))
+        dt = []
+        dt.append(new_img_array)
+        X = np.array(dt)
+        X = X/255
+        val = model.predict(X)
+
+        #256 Inception V3
+        new_img_array_2 = cv2.resize(img_array, (224, 224))
+        #new_img_array_2 = img_array.resize((224, 224))
+        dt_2 = []
+        dt_2.append(new_img_array_2)
+        X_2 = np.array(dt_2)
+        X_2 = X_2/255
+        val_2 = model_2.predict(X_2)
+
+        preds = [val, val_2]
+        weights = [0.5,0.5]
+        weighted_preds = np.tensordot(preds, weights, axes=((0),(0)))
+
+
+        if weighted_preds > 0.5:
+            print("COVID")
+            return "1"
+        else:
+            print("NORMAL")
+            return "0"
+
---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request,jsonify
+from urllib.request import urlopen as uReq
+import io
+
+import numpy as np
+from Ensemble import Ensemble
+from PIL import Image
+import cv2
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def infer_image():
+    if 'file' not in request.files:
+        return "Please try again. The Image doesn't exist"
+    
+    file = request.files.get('file')
+
+    if not file:
+        return
+
+    img_bytes = file.read()
+    img = prepare_image(img_bytes)
+
+    return jsonify(prediction=img)
+
+def prepare_image(img):
+    img = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_COLOR)
+    #img = Image.open(io.BytesIO(img))
+    obj = Ensemble()
+    pred = obj.Result(img)
+    return pred
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8000, debug=True)
+	#app.run(debug=True)
