f90cdcf0fb9d7da09b8abb5e42d7897882d5934f
bhanuteja2001 <k.bhanuteja2001@gmail.com>
2022-05-13 18:41:07 +0530

    Chest or random image check

--- Ensemble.py
+++ Ensemble.py
@@ -6,6 +6,7 @@ import os
 
 
 MODEL_PTH = os.getcwd()
+model_check = load_model(MODEL_PTH+"/weights/VGG16_CL.h5")
 model = load_model(MODEL_PTH+"/weights/Dense_net.h5")
 model_2 = load_model(MODEL_PTH+"/weights/incpetion_v3.h5")
 
@@ -17,28 +18,42 @@ class Ensemble:
         #plt.imshow(img_array)
         #plt.show()
         
-        #128 DenseNet
+
         new_img_array = cv2.resize(img_array, (224, 224))
         #new_img_array = img_array.resize((128,128))
         dt = []
         dt.append(new_img_array)
         X = np.array(dt)
         X = X/255
-        val = model.predict(X)
+        val = model_check.predict(X)
+        Rs = np.argmax(val,axis=1)[0]
+        print("Prediction L:: ",Rs, max(val[0]))
 
-        #256 Inception V3
-        new_img_array_2 = cv2.resize(img_array, (224, 224))
-        #new_img_array_2 = img_array.resize((224, 224))
-        dt_2 = []
-        dt_2.append(new_img_array_2)
-        X_2 = np.array(dt_2)
-        X_2 = X_2/255
-        val_2 = model_2.predict(X_2)
+        if Rs == 0 and max(val[0]) > 0.75:
+            #224 DenseNet
+            new_img_array = cv2.resize(img_array, (224, 224))
+            #new_img_array = img_array.resize((128,128))
+            dt = []
+            dt.append(new_img_array)
+            X = np.array(dt)
+            X = X/255
+            val = model.predict(X)
 
-        preds = [val, val_2]
-        weights = [0.5,0.5]
-        weighted_preds = np.tensordot(preds, weights, axes=((0),(0)))
+            #256 Inception V3
+            new_img_array_2 = cv2.resize(img_array, (224, 224))
+            #new_img_array_2 = img_array.resize((224, 224))
+            dt_2 = []
+            dt_2.append(new_img_array_2)
+            X_2 = np.array(dt_2)
+            X_2 = X_2/255
+            val_2 = model_2.predict(X_2)
 
+            preds = [val, val_2]
+            weights = [0.5,0.5]
+            weighted_preds = np.tensordot(preds, weights, axes=((0),(0)))
 
-        return str(np.argmax(weighted_preds,axis=1))
+            
+            return str(np.argmax(weighted_preds,axis=1))
+        else:
+            return str("Please upload a correct Image")
 
--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def prepare_image(img):
 
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8000, debug=True)
-	app.run(debug=True)
+	app.run(debug=True,threaded=True)
